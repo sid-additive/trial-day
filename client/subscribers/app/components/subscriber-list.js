036b6702c16d476bd2fb7b7c8b83d49b6d4051a2
Robert Bierbauer <robert.bierbauer@additive.eu>
2021-05-19 17:19:57 +0200

    Style subscriber list

--- client/subscribers/app/components/subscriber-list.js
+++ client/subscribers/app/components/subscriber-list.js
@@ -1,5 +1,5 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class SubscriberListComponent extends Component {
   @tracked
@@ -10,25 +10,33 @@ export default class SubscriberListComponent extends Component {
 
     this.subscribers = [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "john.doe@mail.com",
+        firstName: 'John',
+        lastName: 'Doe',
+        gender: 'm',
+        email: 'john.doe@mail.com',
+        state: 'subscribed'
       },
       {
-        firstName: "Jane",
-        lastName: "Doe",
-        email: "jane.doe@mail.com",
+        firstName: 'Jane',
+        lastName: 'Doe',
+        gender: 'f',
+        email: 'jane.doe@mail.com',
+        state: 'subscribed'
       },
       {
-        firstName: "Mary",
-        lastName: "Rose",
-        email: "mary.rose@mail.com",
+        firstName: 'Mary',
+        lastName: 'Rose',
+        gender: 'f',
+        email: 'mary.rose@mail.com',
+        state: 'unsubscribed'
       },
       {
-        firstName: "Paul",
-        lastName: "Smith",
-        email: "paul.smith@mail.com",
-      },
+        firstName: 'Paul',
+        lastName: 'Smith',
+        gender: 'm',
+        email: 'paul.smith@mail.com',
+        state: 'subscribed'
+      }
     ];
   }
 }
