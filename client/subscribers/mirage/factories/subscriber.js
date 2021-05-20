f810fd7c62c10bdf1e61da287f8901bfbbd59180
Robert Bierbauer <robert.bierbauer@additive.eu>
2021-05-20 15:15:37 +0200

    Fix subscriber factory

--- client/subscribers/mirage/factories/subscriber.js
+++ client/subscribers/mirage/factories/subscriber.js
@@ -2,9 +2,9 @@ import { Factory } from 'ember-cli-mirage';
 import faker from 'faker';
 
 export default Factory.extend({
-  email: faker.internet.email(),
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
-  gender: faker.random.arrayElement(['m', 'f']),
-  subscribed: faker.random.boolean
+  email: () => faker.internet.email(),
+  firstName: () => faker.name.firstName(),
+  lastName: () => faker.name.lastName(),
+  gender: () => faker.random.arrayElement(['m', 'f']),
+  subscribed: () => faker.random.boolean()
 });
